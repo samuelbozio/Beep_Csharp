3d2038095aea517a9d59633de4e6c9f1889465a6
Samuel Bozio <samuel@rodosoft.com.br>
2023-10-19 14:17:15 -0300

    bip

---
+++ Beep/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Digite uma frequência? Hz");
+int Frequency = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite uma duração? Ms");
+int Duration = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite [A] para dar Play");
+var a = Console.ReadLine();
+
+if (a == "A")
+{
+    Console.Beep(frequency: Frequency, duration: Duration);
+}
+else
+{
+    Console.WriteLine("Digite pelo menos a tecla certa...");
+}
+
